149ae5c9af2c7e2143e0da765adfb0b2e316dd40
Andy <desac216@gmail.com>
2017-05-05 10:36:08 -0500

    Limpiando comentarios

--- src/main/java/pe/org/ac/siges/util/Constant.java
+++ src/main/java/pe/org/ac/siges/util/Constant.java
@@ -10,29 +10,5 @@ public class Constant {
 	public static final String MNSJ_UPDATE = "Se ha modificado correctamente";	
 	public static final String MNSJ_SAVE = "Se ha procesado correctamente";
 	public static final String MNSJ_USO = "El registro se encuentra en uso";
-	/*public static final String MNSJ_PROC_ELIM = "El registro ya fue procesado, no se puede eliminar";
-	public static final String MNSJ_PROCE = "El registro ya fue procesado";
-	public static final String FRMT_CARGA = "Carga";
-	public static final String ERROR_GRABA_ARCHIVO="Verifique el archivo o intente cargarlo en unos momentos";
-	public static final String ERROR_TIPO_ARCHIVO ="El tipo de archivo es incorrecto";
-	public static final Integer CARGA = 0;
-	public static final Integer CARGA_OK = 1;
-	public static final Integer CARGA_KO = 2;
-	public static final Integer CARGA_VALI = 3;
-	public static final Integer CARGA_NOVALI = 4;
-	public static final Integer CARGA_PROCESADO = 5;
-	public static final String MNSJ_DECI = "El valor es incorrecto, debe tener 2 decimales";
-	public static final String MNSJ_ESTA = "El estado del item es incorrecto";
-	public static final Integer CARGA_MOVIL = 1;
-	public static final Integer CARGA_EXCEL = 2;
-	public static final Integer CARGA_MANUAL = 3;
-	public static final String ESTA_B ="BUENO";
-	public static final String ESTA_M ="MALO";
-	public static final String ESTA_R ="REGULAR";*/
-	//public static final String DATA_VALI_DELE = "2";
-	//public static final String MSJ_VALI_DELE = "El registró se encuentra en uso";
-	//public static final String DATA_VALI_DUPL = "3";
-	//public static final String MSJ_VALI_DUPL = "La información ya fue registrada";
-	
 
 }
